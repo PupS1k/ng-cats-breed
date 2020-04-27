49da8d14bba6681c68005264b78010dce856221e
YTeH0k <denikpro@mail.ru>
2020-04-28 02:00:02 +0300

    add  sort for deleted item in list

--- src/app/pipes/sortList.pipe.ts
+++ src/app/pipes/sortList.pipe.ts
@@ -4,8 +4,15 @@ import {Item} from '../models/item.model';
 @Pipe({name: 'sortList'})
 export class SortListPipe implements PipeTransform {
   transform(value: Item[]) {
-    return value.sort((first, second) => {
+    const sortedList = value.sort((first, second) => {
       return first.name.toLowerCase() > second.name.toLowerCase() ? 1 : -1;
+    });
+
+    return sortedList.sort((first, second) => {
+      if (first.isDeleted === second.isDeleted) {
+        return 0;
+      }
+      return first.isDeleted && !second.isDeleted ? 1 : -1;
     });
   }
 }
