4b09f06548abc55f8c5e24d0c1374be63aff7c91
YTeH0k <denikpro@mail.ru>
2020-04-28 01:43:47 +0300

    setting deleted item and his routing

--- src/app/components/list-item/list-item-smart.component.ts
+++ src/app/components/list-item/list-item-smart.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Item} from '../../models/item.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list-item-smart',
@@ -15,7 +16,20 @@ export class ListItemSmartComponent {
 
   @Output() deleteItem = new EventEmitter();
 
+  constructor(private router: Router) {
+  }
+
+  getCurrentItemId() {
+    const url =  this.router.url;
+    return +url.slice(url.lastIndexOf('/') + 1, url.length);
+  }
+
   onDeleteItem(id) {
+    const currentItemId = this.getCurrentItemId();
+    if (currentItemId === id) {
+      this.router.navigate(['/']);
+    }
+
     this.deleteItem.emit(id);
   }
 }
